ebae325fabfa0e47a8d926500f95bcb439cf8024
aussieW <austin.j.wilson@gmail.com>
2018-03-12 06:00:16 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -127,11 +127,10 @@ class mqttskill(MycroftSkill):
         LOGGER.info('AJW: handle show world time')
 	
 	# ask for the city to display
-	response = self.get_response('which city ould you like to show')
-		
+	response = self.get_response('ask.for.city')
         city = response
+	
         loc_name = message.data.get("LocationKeyword")
-
         # set a default location in none provided
         if loc_name:
             loc_name = loc_name.replace(' ', '_')
@@ -141,11 +140,11 @@ class mqttskill(MycroftSkill):
         self.mqtt_connect(actionConfirmationTopic)
         LOGGER.info("AJW - connect to: " + self.mqtthost)
         LOGGER.info("AJW - connect to: " + str(self.mqttport))
-        self.mqttc_publish(loc_name + "/display/worldtime", city)
+        self.mqtt_publish(loc_name + "/display/worldtime", city)
         self.speak_dialog("cmd.sent")
         # wait for a response before disconnecting
         time.sleep(10)
-        self.mqttc_disconnect()
+        self.mqtt_disconnect()
 
     @intent_handler(IntentBuilder('').require("ShowWorldTime").require("WorldTime").require("city_name").optionally("LocationKeyword"))
     def handle_show_world_time_city(self, message):
@@ -164,11 +163,11 @@ class mqttskill(MycroftSkill):
         self.mqtt_connect(actionConfirmationTopic)
         LOGGER.info("AJW - connect to: " + self.mqtthost)
         LOGGER.info("AJW - connect to: " + str(self.mqttport))
-        self.mqttc_publish(loc_name + "/display/worldtime", city)
+        self.mqtt_publish(loc_name + "/display/worldtime", city)
         self.speak_dialog("cmd.sent")
         # wait for a response before disconnecting
         time.sleep(10)
-        self.mqttc_disconnect()
+        self.mqtt_disconnect()
 
     @intent_handler(IntentBuilder('handle_hide_world_time').require("HideWordTime").require("WorldTime").optionally("LocationKeyword"))
     def handle_hide_world_time(self, message):
